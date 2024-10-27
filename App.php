b079966989783d0a2914bd6813b5d883664642ce
egido-quan <isidro.zapater@gmail.com>
2024-10-27 11:21:01 +0100

    Se pide al usuario dimensión y tokens

--- App.php
+++ App.php
@@ -1,34 +1,44 @@
 <?php
 include "TresEnRaya.php";
 
-const TIC_TAC_TOE = 3; //TIC TAC TOE siempre es de 3
 const TOKEN_X = "X";
 const TOKEN_O = "O";
 
-const DIMENSION = TIC_TAC_TOE; //Para 3 poner TIC_TAC_TOE, para otras dimensiones poner el número
-
-function token() {
-        return (rand(0,1) == 0) ? TOKEN_X : TOKEN_O;
+function token( $tokenX, $tokenO) {
+        return (rand(0,1) == 0) ? $tokenX : $tokenO;
 }
 
 
-function tirada () {
+function tirada ($dimension, $tokenX, $tokenO) {
         $matriz = [];
-        for($j = 0; $j < DIMENSION; $j ++) {     
-                for($i = 0; $i < DIMENSION; $i ++) {
-                        $matriz [$j][$i] = token();
+        for($j = 0; $j < $dimension; $j ++) {     
+                for($i = 0; $i < $dimension; $i ++) {
+                        $matriz [$j][$i] = token( $tokenX, $tokenO);
                 }
         }
         return $matriz;
 }
 
+echo "VAMOS A JUGAR A TIC_TAC_TOE!!" . PHP_EOL;
 
+$dos = false;
+while (!$dos) {
+        $tokens = readline("Qué símbolos usarás para jugar (introdúce DOS SÍMBOLOS separados por un espacio) ? ");
+        $dos = count(explode(' ', $tokens)) == 2;
+        }
+list($tokenX, $tokenO) = explode(' ', $tokens);
+$dimension = -1;
+while ($dimension !=0) {
+        $dimension = readline("Introduce la dimensión del TIC_TAC_TOE (ej: 3 para 3x3) o cualquier otra tecla para dejar de jugar: ");
+        $dimension = (int)$dimension;
+        if ($dimension != 0) {
+                $tirada = tirada($dimension, $tokenX, $tokenO);
+                $jugada = new TresEnRaya($tirada, $dimension, $tokenX, $tokenO);
+                echo PHP_EOL . $jugada->evaluarPartida() . PHP_EOL . PHP_EOL;
+        }
+}
 
-$tirada1 = tirada();
-$jugada1 = new TresEnRaya($tirada1, DIMENSION, TOKEN_X, TOKEN_O);
-echo PHP_EOL . $jugada1->evaluarPartida() . PHP_EOL . PHP_EOL;
-
-
+echo PHP_EOL . "Hasta pronto!";
 
 
 
